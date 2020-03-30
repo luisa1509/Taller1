6d14a16b601a4793870c4586575ff82cbf3a6513
Usuario <luisa_15092000@hotmail.com>
2020-03-29 19:37:25 -0500

    actualizado

--- app/src/main/java/com/example/taller1/JuegoPrincipal.java
+++ app/src/main/java/com/example/taller1/JuegoPrincipal.java
@@ -10,7 +10,10 @@ public class JuegoPrincipal extends AppCompatActivity {
 
     private Button correr;
     private Button accion;
-    private ImageView conejo;
+    private Button arriba;
+    private Button abajo;
+    private Button izquierda;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,49 @@ public class JuegoPrincipal extends AppCompatActivity {
 
         correr = findViewById(R.id.correr);
         accion = findViewById(R.id.accion);
-        conejo = findViewById(R.id.conejo);
+        arriba = findViewById(R.id.arriba);
+        abajo = findViewById(R.id.abajo);
+        izquierda = findViewById(R.id.izquierda);
+
+
+
+        correr.setOnClickListener(
+                (v)->{
+
+                    comm.mandarMensaje("POS::RIGHT");
+                }
+        );
+
+        accion.setOnClickListener(
+                (v)->{
+
+                    comm.mandarMensaje("POS::recarga");
+                }
+        );
+
+        arriba.setOnClickListener(
+                (v)->{
+
+                    comm.mandarMensaje("POS::UP");
+                }
+        );
+
+        abajo.setOnClickListener(
+                (v)->{
+
+                    comm.mandarMensaje("POS::DOWN");
+                }
+        );
+
+        izquierda.setOnClickListener(
+                (v)->{
+
+                    comm.mandarMensaje("POS::LEFT");
+                }
+        );
+
+
+
 
 
     }
